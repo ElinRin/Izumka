4c1428ca664342c26a0a9cd59d1b462529d8afd6
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2017-11-26 06:30:20 +0300

    06:29 Misha sleeps

--- src/containers/info/index.js
+++ src/containers/info/index.js
@@ -2,22 +2,63 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import classNames from 'classnames';
 
-import {fetch} from '../../actions';
+import {getData} from '../../actions';
 
 import './style.css'
 
 class Info extends Component {
+    componentDidMount() {
+        console.log("bly");
+        console.log(this.props.info);
+        this.props.getData("");
+        console.log("bly");
+        console.log(this.props.info);
+    }
 
-    renderOrganizations(organization) {
+    renderOrganizations(organizations) {
+        let values = [];
+    
+        for(let org in organizations) {
+            values.push(org);
+        };
+
         return (
-            <tr>
-                <th>
-                    <td>{organization.org}</td>
-                    <td>{organization.date}</td>
-                    <td>{organization.qua}</td>
-                </th>
-            </tr>
+            values.map((value, index) =>  (
+                        <tr key={index}>
+                            <td>{index}</td>
+                            <td>{value.org}</td>
+                            <td>{value.date}</td>
+                            <td>{value.qua}</td>
+                        </tr>
+            ))
         )
+    }
+
+    renderFields() {
+
+    const {info} = this.props;
+    return (
+        <div className="div-center">
+        <h2>Информация о квалификации</h2>
+        <table className="table table-hover mytable">
+            <thead>
+                <tr>
+                <th colSpan='4' className="table-head">{info.name}</th>
+                </tr>
+                <tr>
+                <th>#</th>
+                <th>Организация</th>
+                <th>Даты</th>
+                <th>Квалификация</th>
+                </tr>
+            </thead>
+            <h2>{info.name}</h2>
+            <tbody>
+                    {info.organizations && this.renderOrganizations(info.organizations)}
+            </tbody>
+            </table>
+        </div>
+    )
     }
 
     render ( ) {
@@ -27,46 +68,22 @@ class Info extends Component {
                 <div className="row">
                 <div className="col-md-2"></div>
                 <div className="col-md-8">
-                <div className="div-center">
-                <h2>Информация о квалификации</h2>
-                <table className="table table-hover mytable">
-                    <thead>
-                        <tr>
-                        <th colSpan='4' className="table-head">{info.name}</th>
-                        </tr>
-                        <tr>
-                        <th>#</th>
-                        <th>Организация</th>
-                        <th>Даты</th>
-                        <th>Квалификация</th>
-                        </tr>
-                    </thead>
-                    <h2>{info.name}</h2>
-                    <tbody>
-                    {
-                        Object.keys(info.organizations).map((org, index) => ( 
-                            <div key={index}>
-                                {this.renderOrganizations(org)}
-                            </div>
-                        ))
-                    }
-                    </tbody>
-                    </table>
-                </div>
+                {info.organizations && this.renderFields()}
                 </div>
                 <div className="col-md-2"></div>
                 </div>
             </div>
-        )
-    };
+    )
+    }
 };
 
 const mapStateToProps = state => {
+    console.log(state)
     return {
         info: state.info
     }
 };
   
-const mapDispatchToProps = {fetch};
+const mapDispatchToProps = {getData};
   
 export default connect(mapStateToProps, mapDispatchToProps)(Info);
