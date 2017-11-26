ab2ea23070547c7aafc8bf8c08d285a4d0de0f83
ElinRin <elin.rinnel@gmail.com>
2017-11-26 12:55:48 +0300

    add return button on all pages

--- src/containers/complete/index.js
+++ src/containers/complete/index.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
 
+import Return from '../return';
+
 class Complete extends Component {
 
     render () {
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <Return/>
+                    </div>
+                </div>
                 <div className="row">
                  <div className="col-md-2"></div>
                  <div className="col-md-8">
