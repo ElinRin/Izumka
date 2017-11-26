8b311396f630bbcc2f16f8f8cccee4f2f38fe6f4
xmanatee <xmanatee@Artsems-MacBook-Pro.local>
2017-11-26 06:50:02 +0300

    123

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,17 @@
+var Web3 = require('web3');
+var web3 = new Web3();
+
+var web3 = new Web3('http://localhost:8545');
+
+// web3.setProvider(new web3.providers.HttpProvider());
+
 export const getDataApi = async () => {
     const URL = '';
     return fetch(URL).then(res => res.json());
 };
 
 export const postDataApi = async () => {
-    const URL = '';
+    // const URL = '';
     return fetch('url', {
         method: 'POST',
         body: 'foo=bar'
